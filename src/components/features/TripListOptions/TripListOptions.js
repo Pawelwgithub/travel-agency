3db2e03da1c13fe03d418ae124a1789af947b7af
Paweł Walkowski <walkowski.pawel@wp.pl>
2020-06-16 23:32:11 +0200

    Module 13.5. Trips list filtering

--- src/components/features/TripListOptions/TripListOptions.js
+++ src/components/features/TripListOptions/TripListOptions.js
@@ -7,9 +7,11 @@ import {Row, Col} from 'react-flexbox-grid';
 class TripListOptions extends React.Component {
   handleTags(tag, checked){
     if(checked) {
+      this.props.addTag(tag);
       console.log('Adding tag', tag);
       // TODO - use action dispatcher from props
     } else {
+      this.props.removeTag(tag);
       console.log('Removing tag', tag);
       // TODO - use action dispatcher from props
     }
@@ -18,6 +20,11 @@ class TripListOptions extends React.Component {
   handleDuration(type, value){
     console.log('Changing duration', type, value);
     // TODO - use action dispatcher from props
+    if (type === 'from') {
+      this.props.changeFromDuration(parseInt(value));
+    } else {
+      this.props.changeToDuration(parseInt(value));
+    }
   }
 
   handleSearch(phrase){
@@ -73,6 +80,10 @@ TripListOptions.propTypes = {
   tags: PropTypes.object,
   filters: PropTypes.object,
   changeSearchPhrase: PropTypes.func,
+  changeFromDuration: PropTypes.func,
+  changeToDuration: PropTypes.func,
+  addTag: PropTypes.func,
+  removeTag: PropTypes.func,
 };
 
 export default TripListOptions;
