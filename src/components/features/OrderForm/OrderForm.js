6296a4043d73a3fcd9509770e1ed349d53e132fa
Paweł Walkowski <walkowski.pawel@wp.pl>
2021-02-17 22:21:32 +0100

    Module 15.4. Change order form. Add button and sendOrder function for order form

--- src/components/features/OrderForm/OrderForm.js
+++ src/components/features/OrderForm/OrderForm.js
@@ -3,9 +3,40 @@ import PropTypes from 'prop-types';
 import OrderSummary from '../OrderSummary/OrderSummary';
 import OrderOption from '../OrderOption/OrderOption';
 import pricing from '../../../data/pricing.json';
+import Button from '../../common/Button/Button';
+import { formatPrice } from '../../../utils/formatPrice';
+import { calculateTotal } from '../../../utils/calculateTotal';
+import settings from '../../../data/settings';
 
 //import styles from './OrderForm.scss';
 import { Row, Col } from 'react-flexbox-grid';
+
+const sendOrder = (options, tripCost) => {
+  const totalCost = formatPrice(calculateTotal(tripCost, options));
+
+  const payload = {
+    ...options,
+    totalCost,
+  };
+
+  const url = settings.db.url + '/' + settings.db.endpoint.orders;
+
+  const fetchOptions = {
+    cache: 'no-cache',
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(payload),
+  };
+
+  fetch(url, fetchOptions)
+    .then(function(response){
+      return response.json();
+    }).then(function(parsedResponse){
+      console.log('parsedResponse', parsedResponse);
+    });
+};
 
 //const OrderForm = ({ tripCost, options }) => (
 const OrderForm = ({ tripCost, options, setOrderOption }) => (
@@ -18,6 +49,7 @@ const OrderForm = ({ tripCost, options, setOrderOption }) => (
     )}
     <Col xs={12}>
       <OrderSummary tripCost={tripCost} options={options} />
+      <Button onClick={() => sendOrder(options, tripCost)}>Order now!</Button>
     </Col>
   </Row>
 );
